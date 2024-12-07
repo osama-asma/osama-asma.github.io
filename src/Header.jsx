bb4d68b27a88dc62b7cc72d22b59195c17f3e0dc
Osama Dahnoun <187145138+osamadahnoun-cisco@users.noreply.github.com>
2024-12-07 09:53:25 -0500

    fix

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,55 +1,54 @@
-import React, { useEffect, useState } from 'react'
-import { NavLink, useLocation, useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
-import Modal from './Modal';
-import config from './config'
+import React, { useEffect, useState } from "react";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import Modal from "./Modal";
+import config from "./config";
 
 const StyledNavLink = styled(NavLink)`
-text-decoration: none;
-color: #fff;
-padding: 8px;
-margin: 0 10px;
-border-bottom: 2px solid transparent;
+  text-decoration: none;
+  color: #fff;
+  padding: 8px;
+  margin: 0 10px;
+  border-bottom: 2px solid transparent;
 
-&:hover {
-  border-bottom: 2px solid #fff;
-}
+  &:hover {
+    border-bottom: 2px solid #fff;
+  }
 
-&.active {
-  border-bottom: 2px solid #fff;
-}
+  &.active {
+    border-bottom: 2px solid #fff;
+  }
 
-@media (max-width: 768px) {
-  margin: 5px 0;
-  text-align: center;
-  width: 100%;
-}
+  @media (max-width: 768px) {
+    margin: 5px 0;
+    text-align: center;
+    width: 100%;
+  }
 `;
 
 const HamburgerButton = styled.button`
-display: none;
+  display: none;
 
-@media (max-width: 768px) {
-  display: block;
-  background: none;
-  border: none;
-  color: #fff;
-  font-size: 1.5rem;
-  cursor: pointer;
-}
+  @media (max-width: 768px) {
+    display: block;
+    background: none;
+    border: none;
+    color: #fff;
+    font-size: 1.5rem;
+    cursor: pointer;
+  }
 `;
 
-
 const PartyHeaderContainer = styled.div`
-background-color: #e0b8a6;
-color: #fff;
-padding: 10px;
+  background-color: #e0b8a6;
+  color: #fff;
+  padding: 10px;
 `;
 
 const KatibHeaderContainer = styled.div`
-background-color: #E64C3C;
-color: #fff;
-padding: 10px;
+  background-color: #e64c3c;
+  color: #fff;
+  padding: 10px;
 `;
 
 const NavLinksContainer = styled.div`
@@ -59,124 +58,143 @@ const NavLinksContainer = styled.div`
   @media (max-width: 768px) {
     flex-direction: column;
     align-items: center;
-    display: ${({ isOpen }) => (isOpen ? 'flex' : 'none')};
+    display: ${({ isOpen }) => (isOpen ? "flex" : "none")};
   }
 `;
-const Header = ({ setIsLoggedIn, setIsAuthed, isAuthed, justSubmitted, setJustSubmitted }) => {
-    const [isOpen, setIsOpen] = useState(false);
-    const location = useLocation()
-    const partyPage = location.pathname === '/party'
-    const navigate = useNavigate()
+const Header = ({
+  setIsLoggedIn,
+  setIsAuthed,
+  isAuthed,
+  justSubmitted,
+  setJustSubmitted,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const location = useLocation();
+  const partyPage = location.pathname === "/party";
+  const navigate = useNavigate();
 
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
-    };
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
-    const [showModal, setShowModal] = useState(false)
-    // State to manage the input value
-    const [inputValue, setInputValue] = useState('');
-    const [incorrectPassword, setIncorrectPassword] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  // State to manage the input value
+  const [inputValue, setInputValue] = useState("");
+  const [incorrectPassword, setIncorrectPassword] = useState(false);
 
+  // Event handler to update the state when the input changes
+  const handleInputChange = (event) => {
+    setIncorrectPassword(false);
+    setInputValue(event.target.value);
+  };
 
-    // Event handler to update the state when the input changes
-    const handleInputChange = (event) => {
-        setIncorrectPassword(false)
-        setInputValue(event.target.value);
-    };
+  const handleSubmit = (value) => {
+    if (
+      value.trim() === config.quantumPenguin ||
+      value.trim() === config.moonGiraffe
+    ) {
+      sessionStorage.setItem("isAuthed", "true");
+      setIsAuthed(true);
+      setShowModal(false);
+      setJustSubmitted(true); // Add a state to track recent submission
+    } else {
+      setIncorrectPassword(true);
+    }
+  };
 
-    const handleSubmit = (value) => {
-        if (value === config.quantumPenguin || value === config.moonGiraffe) {
-            sessionStorage.setItem('isAuthed', 'true');
-            setIsAuthed(true);
-            setShowModal(false);
-            setJustSubmitted(true); // Add a state to track recent submission
-        } else {
-            setIncorrectPassword(true);
-        }
-    };
+  const handleLogout = () => {
+    sessionStorage.removeItem("isLoggedIn");
+    sessionStorage.removeItem("isAuthed");
+    sessionStorage.removeItem("isUnlocked");
+    setIsLoggedIn(false);
+    setIsAuthed(false);
+    setJustSubmitted(false);
+  };
 
-    const handleLogout = () => {
-        sessionStorage.removeItem('isLoggedIn');
-        sessionStorage.removeItem('isAuthed');
-        sessionStorage.removeItem('isUnlocked');
-        setIsLoggedIn(false);
-        setIsAuthed(false)
-        setJustSubmitted(false)
-    };
+  useEffect(() => {
+    if (isAuthed && justSubmitted) {
+      navigate("/auth");
+      setJustSubmitted(false); // Reset the state after navigation
+    }
+  }, [isAuthed, justSubmitted, navigate, setJustSubmitted]);
 
-    useEffect(() => {
-        if (isAuthed && justSubmitted) {
-            navigate('/auth');
-            setJustSubmitted(false); // Reset the state after navigation
-        }
-    }, [isAuthed, justSubmitted, navigate, setJustSubmitted]);
+  return (
+    <>
+      {showModal && (
+        <Modal
+          value={inputValue}
+          setValue={handleInputChange}
+          showModal={setShowModal}
+          buttonText={"Submit"}
+          handleSubmit={handleSubmit}
+          incorrectPassword={incorrectPassword}
+        />
+      )}
+      {partyPage ? (
+        <PartyHeaderContainer>
+          <HamburgerButton onClick={toggleMenu}>&#9776;</HamburgerButton>
+          <NavLinksContainer isOpen={isOpen}>
+            <StyledNavLink to="/home" exact activeClassName="active">
+              Home
+            </StyledNavLink>
+            <StyledNavLink to="/kitab" activeClassName="active">
+              Katib Kitab
+            </StyledNavLink>
+            <StyledNavLink
+              activeClassName="active"
+              to="/party"
+              onClick={(e) => {
+                e.preventDefault();
+                if (sessionStorage.getItem("isAuthed") === "true") {
+                  setIsAuthed(true);
+                  navigate("/auth");
+                } else {
+                  setInputValue("");
+                  setShowModal(true);
+                }
+              }}
+            >
+              Engagement Party
+            </StyledNavLink>
+            <StyledNavLink to="/login" onClick={handleLogout}>
+              Logout
+            </StyledNavLink>
+          </NavLinksContainer>
+        </PartyHeaderContainer>
+      ) : (
+        <KatibHeaderContainer>
+          <HamburgerButton onClick={toggleMenu}>&#9776;</HamburgerButton>
+          <NavLinksContainer isOpen={isOpen}>
+            <StyledNavLink to="/home" exact activeClassName="active">
+              Home
+            </StyledNavLink>
+            <StyledNavLink to="/kitab" activeClassName="active">
+              Katib Kitab
+            </StyledNavLink>
+            <StyledNavLink
+              activeClassName="active"
+              to="/party"
+              onClick={(e) => {
+                e.preventDefault();
+                if (sessionStorage.getItem("isAuthed") === "true") {
+                  setIsAuthed(true);
+                  navigate("/auth");
+                } else {
+                  setInputValue("");
+                  setShowModal(true);
+                }
+              }}
+            >
+              Engagement Party
+            </StyledNavLink>
+            <StyledNavLink to="/login" onClick={handleLogout}>
+              Logout
+            </StyledNavLink>
+          </NavLinksContainer>
+        </KatibHeaderContainer>
+      )}
+    </>
+  );
+};
 
-
-    return (
-        <>
-            {showModal && <Modal value={inputValue} setValue={handleInputChange} showModal={setShowModal} buttonText={'Submit'} handleSubmit={handleSubmit} incorrectPassword={incorrectPassword} />}
-            {partyPage ? (
-                <PartyHeaderContainer>
-                    <HamburgerButton onClick={toggleMenu}>&#9776;</HamburgerButton>
-                    <NavLinksContainer isOpen={isOpen}>
-                        <StyledNavLink to="/home" exact activeClassName="active">
-                            Home
-                        </StyledNavLink>
-                        <StyledNavLink to="/kitab" activeClassName="active">
-                            Katib Kitab
-                        </StyledNavLink>
-                        <StyledNavLink activeClassName="active" to="/party" onClick={(e) => {
-                            e.preventDefault()
-                            if (sessionStorage.getItem('isAuthed') === 'true') {
-                                setIsAuthed(true)
-                                navigate('/auth')
-                            }
-                            else {
-                                setInputValue('')
-                                setShowModal(true)
-                            }
-                        }}  >
-                            Engagement Party
-                        </StyledNavLink>
-                        <StyledNavLink to="/login" onClick={handleLogout}>
-                            Logout
-                        </StyledNavLink>
-                    </NavLinksContainer>
-                </PartyHeaderContainer>
-            ) :
-                (
-                    <KatibHeaderContainer>
-                        <HamburgerButton onClick={toggleMenu}>&#9776;</HamburgerButton>
-                        <NavLinksContainer isOpen={isOpen}>
-                            <StyledNavLink to="/home" exact activeClassName="active">
-                                Home
-                            </StyledNavLink>
-                            <StyledNavLink to="/kitab" activeClassName="active">
-                                Katib Kitab
-                            </StyledNavLink>
-                            <StyledNavLink activeClassName="active" to="/party" onClick={(e) => {
-                                e.preventDefault()
-                                if (sessionStorage.getItem('isAuthed') === 'true') {
-                                    setIsAuthed(true)
-                                    navigate('/auth')
-                                }
-                                else {
-                                    setInputValue('')
-                                    setShowModal(true)
-                                }
-                            }}  >
-                                Engagement Party
-                            </StyledNavLink>
-                            <StyledNavLink to="/login" onClick={handleLogout}>
-                                Logout
-                            </StyledNavLink>
-                        </NavLinksContainer>
-                    </KatibHeaderContainer>
-                )
-            }
-
-        </>
-    )
-}
-
-export default Header
+export default Header;
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,75 +1,80 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
-import config from './config';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import config from "./config";
 
 const StyledButton = styled.button`
-    padding: 10px;
-    margin-top: 16px;
-    background-color: #394867;
-    color: #c0c0c0;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-    font-weight: bold;
-    &:hover {
-        opacity: 90%;
-
-    }
+  padding: 10px;
+  margin-top: 16px;
+  background-color: #394867;
+  color: #c0c0c0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+  &:hover {
+    opacity: 90%;
+  }
 `;
 
 const Login = ({ setIsLoggedIn, isLoggedIn }) => {
-    const [password, setPassword] = useState('');
-    const [wrong, setWrong] = useState(false);
+  const [password, setPassword] = useState("");
+  const [wrong, setWrong] = useState(false);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        if (password === config.bananaCactus || password === config.stellarRainbow) {
-            sessionStorage.setItem('isLoggedIn', 'true');
-            setIsLoggedIn(true);
-        } else {
-            setWrong(true)
-        }
-    };
+    if (
+      password.trim() === config.bananaCactus ||
+      password.trim() === config.stellarRainbow
+    ) {
+      sessionStorage.setItem("isLoggedIn", "true");
+      setIsLoggedIn(true);
+    } else {
+      setWrong(true);
+    }
+  };
 
-    useEffect(() => {
-        if (isLoggedIn) {
-            navigate('/home');
-        }
-    }, [isLoggedIn, navigate]);
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate("/home");
+    }
+  }, [isLoggedIn, navigate]);
 
-    return (
-        <div>
-            <div className="login-form-container">
-                <form className="login-form" onSubmit={handleSubmit}>
-                    <h2>Password:</h2>
-                    <div style={{ marginTop: '16px' }}>
-                        <label>
-                            <input
-                                type="text"
-                                value={password}
-                                onChange={(e) => {
-                                    setWrong(false)
-                                    setPassword(e.target.value)
-                                }}
-                                style={{ touchAction: 'manipulation' }}
-                            />
-                        </label>
-                    </div>
+  return (
+    <div>
+      <div className="login-form-container">
+        <form className="login-form" onSubmit={handleSubmit}>
+          <h2>Password:</h2>
+          <div style={{ marginTop: "16px" }}>
+            <label>
+              <input
+                type="text"
+                value={password}
+                onChange={(e) => {
+                  setWrong(false);
+                  setPassword(e.target.value);
+                }}
+                style={{ touchAction: "manipulation" }}
+              />
+            </label>
+          </div>
 
-                    {wrong && <>
-                        <div>Wrong password Asma 🤭</div>
-                        <div style={{ color: 'red' }}>If this isn't asma, leave now! 😡👮🏽‍♀️🚔</div>
-                    </>}
-                    <StyledButton type='submit'>Log in</StyledButton>
-                </form>
-            </div>
-        </div>
-
-    );
+          {wrong && (
+            <>
+              <div>Wrong password Asma 🤭</div>
+              <div style={{ color: "red" }}>
+                If this isn't asma, leave now! 😡👮🏽‍♀️🚔
+              </div>
+            </>
+          )}
+          <StyledButton type="submit">Log in</StyledButton>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
